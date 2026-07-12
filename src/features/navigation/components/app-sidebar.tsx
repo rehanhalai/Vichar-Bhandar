4c08164b8c0c72038d99ae8eb3f6a7053a95e730
Rehan Halai <rehanhalai@192.168.1.8>
2026-07-12 20:03:40 +0530

    refactor - navigation feature

--- src/features/navigation/components/app-sidebar.tsx
+++ src/features/navigation/components/app-sidebar.tsx
@@ -15,9 +15,9 @@ import {
 } from "@tabler/icons-react"
 import { NotificationScheduler } from "@/features/notifications/components/notification-scheduler"
 
-import { NavMain } from "@/components/nav-main"
-import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
+import { NavMain } from "@/features/navigation/components/nav-main"
+import { NavSecondary } from "@/features/navigation/components/nav-secondary"
+import { NavUser } from "@/features/navigation/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
